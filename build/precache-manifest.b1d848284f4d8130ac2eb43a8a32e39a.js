72a2c7911e30cf1be99f13e5de2e62ffa7968299
khushbu <skhushbu761@gmail.com>
2023-08-25 18:27:27 +0530

    another commit

---
+++ build/precache-manifest.b1d848284f4d8130ac2eb43a8a32e39a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779974c5f2ae9be0fc776edbf4a24eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0ed7aa8853d641eeaa",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "9578651078ca109df044",
+    "url": "/static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "0b0ed7aa8853d641eeaa",
+    "url": "/static/js/2.bd0cc2e3.chunk.js"
+  },
+  {
+    "revision": "9578651078ca109df044",
+    "url": "/static/js/main.fd369373.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
